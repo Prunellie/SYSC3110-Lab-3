10f6375431340bc6e62c1c07819bf77123e3c78b
Prunellie Tchakam <prunellie578@gmail.com>
2025-09-17 22:16:42 -0400

    adding and removing a new buddy to the buddy info

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -6,5 +6,9 @@ public class AddressBook {
     public void removeBuddy(BuddyInfo buddy){}
     public static void main(String[] args) {
         System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Katie","657","carleton");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
