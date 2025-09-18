8fe36571c6b787a515f089efca6ae7a3c9ec12c5
Prunellie Tchakam <prunellie578@gmail.com>
2025-09-18 09:39:55 -0400

    adding constructor

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -2,8 +2,13 @@ import java.util.*;
 
 public class AddressBook {
     private ArrayList<BuddyInfo> BuddyList;
-    public void addBuddy(BuddyInfo buddy){}
-    public void removeBuddy(BuddyInfo buddy){}
+
+    public AddressBook() {
+        BuddyList = new ArrayList<>();
+    }
+    public void addBuddy(BuddyInfo buddy){BuddyList.add(buddy);}
+    public void removeBuddy(BuddyInfo buddy){BuddyList.remove(buddy);}
+
     public static void main(String[] args) {
         System.out.println("Address Book");
         BuddyInfo buddy = new BuddyInfo("Katie",657,"carleton");
