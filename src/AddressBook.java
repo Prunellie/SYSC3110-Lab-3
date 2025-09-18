9493541f59d4d1de5138e560da042eae37bd174e
Prunellie Tchakam <prunellie578@gmail.com>
2025-09-17 22:05:42 -0400

    printing "Address Book"

---
+++ src/AddressBook.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> BuddyList;
+    public void addBuddy(BuddyInfo buddy){}
+    public void removeBuddy(BuddyInfo buddy){}
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
