cb1d76afa15ca9fe454c5ae1c530e81affbebe90
Prunellie Tchakam <prunellie578@gmail.com>
2025-09-25 23:43:06 -0400

    refact

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,20 +1,28 @@
 import java.util.*;
 
 public class AddressBook {
-    private ArrayList<BuddyInfo> buddyList;
+    private ArrayList<BuddyInfo> myBuddies;
 
     public AddressBook() {
-        this.buddyList = new ArrayList<>();
+        myBuddies = new ArrayList<>();
     }
-    public void addBuddy(BuddyInfo buddy){this.buddyList.add(buddy);}
-    public void removeBuddy(BuddyInfo buddy){this.buddyList.remove(buddy);}
+    public void addBuddy(BuddyInfo aBuddy){
+        if(aBuddy != null){
+            myBuddies.add(aBuddy);
+        }
+    }
+    public BuddyInfo removeBuddy(int index){
+        if(index >= 0 && index < myBuddies.size()){
+            return myBuddies.remove(index);
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
         BuddyInfo buddy = new BuddyInfo("Katie",657,"carleton");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
-        System.out.println("hello");
+        addressBook.removeBuddy(0);
+        System.out.println("Hello ");
     }
 }
